bed8ac4d8c84c67087099c41bc1875c185de4e36
petereycoder <fp.pipe.reyes@gmail.com>
2022-02-14 15:03:52 -0600

    Protección de rutas

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interfaces';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +16,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //auth!: Auth;
+
+  get auth(){
+    return this.authService.auth;
+  }
+
+  constructor( private router: Router,
+               private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.router.navigate(['./auth']);
   }
 
 }
